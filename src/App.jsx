dd840ffdd395f300eb53504bf9c6fb363c3a8b57
aritrakar19 <110339569+aritrakar19@users.noreply.github.com>
2024-12-06 00:51:38 +0530

    create login, signup, model, cart

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,10 @@ import HomePage from "./pages/home/HomePage";
 import NoPage from "./pages/noPage/NoPage";
 import ProductInfo from "./pages/productInfo/ProductInfo";
 import ScrollTop from "./components/scrollTop/ScrollTop";
+import Login from "./pages/registration/Login";
+import Signup from "./pages/registration/Signup";
+import CartPage from "./pages/cart/CartPage";
+import Dashboard from "./pages/admin/dashboard/Dashboard";
 
 function App() {
   return (
@@ -13,7 +17,11 @@ function App() {
           <ScrollTop />
           <Routes>
             <Route path="/" element={<HomePage />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
             <Route path="/nopage" element={<NoPage />} />
+            <Route path="cart" element={<CartPage />} />
             <Route path="/productinfo" element={<ProductInfo />} />
           </Routes>
         </Router>
